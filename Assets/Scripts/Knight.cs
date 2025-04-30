f937d522b0953b621ce8369897547e186f247561
ahmtgk <gk.ahmet6@gmail.com>
2025-05-01 02:07:44 +0300

    cliff detection

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -9,6 +9,7 @@ public class Knight : MonoBehaviour
     public float walkspeed = 3f;
     public float walkStopRate = 0.05f;
     public DetectionZone attackZone;
+    public DetectionZone cliffDetectionZone;
 
 
     Rigidbody2D rb;
@@ -66,6 +67,19 @@ public class Knight : MonoBehaviour
         }
     }
 
+    public float AttackCooldown 
+    { 
+        get
+        {
+            return animator.GetFloat(AnimationStrings.attackCooldown);
+        } 
+        private set
+        {
+            animator.SetFloat(AnimationStrings.attackCooldown, Mathf.Max(value, 0));
+        } 
+    
+    }
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -77,6 +91,12 @@ public class Knight : MonoBehaviour
     void Update()
     {
         HasTarget = attackZone.detectedColliders.Count > 0;
+
+        if(AttackCooldown > 0)
+        {
+            AttackCooldown -= Time.deltaTime;  
+        }
+       
     }
 
     private void FixedUpdate()
@@ -121,6 +141,14 @@ public class Knight : MonoBehaviour
     public void OnHit(int damage, Vector2 knockback)
     {
         rb.velocity = new Vector2(knockback.x, rb.velocity.y + knockback.y);
+    }
+
+    public void OnCliffDetected()
+    {
+        if (touchingDirections.IsGrounded)
+        {
+            FlipDirection();
+        }
     }
     
 }
