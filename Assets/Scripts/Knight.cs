379af06f603a5d86c474d6759b6f98162964480d
ahmtgk <gk.ahmet6@gmail.com>
2025-05-14 20:24:26 +0300

    attack combo

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections), typeof(Damageable))]
 public class Knight : MonoBehaviour
 {
-    public float walkspeed = 3f;
+    public float walkAcceleration = 3f;
+    public float maxSpeed = 3f;
     public float walkStopRate = 0.05f;
     public DetectionZone attackZone;
     public DetectionZone cliffDetectionZone;
@@ -110,7 +111,7 @@ public class Knight : MonoBehaviour
         {
             if(CanMove)
             {
-                rb.velocity = new Vector2(walkspeed * WalkDirectionVector.x , rb.velocity.y);
+                rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x + (walkAcceleration * WalkDirectionVector.x * Time.fixedDeltaTime), -maxSpeed, maxSpeed), rb.velocity.y);
             }
             else
             {
