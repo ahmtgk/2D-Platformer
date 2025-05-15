e472de93c3ff5c6a968177f5147139a855324f14
ahmtgk <gk.ahmet6@gmail.com>
2025-05-15 21:44:30 +0300

    rangedAttack

---
+++ Assets/ProjectileLauncher.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLauncher : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+
+    public void FireProjectile ()
+    {   
+        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+    }
+    
+}
