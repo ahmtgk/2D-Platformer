bb37aaf9448cb4eea3d4dbc330c46f5748b3f7e5
ahmtgk <gk.ahmet6@gmail.com>
2025-05-26 02:51:03 +0300

    flying eye

---
+++ Assets/FliyngEye.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FliyngEye : MonoBehaviour
+{
+    public float flightSpeed = 2f;
+    public float waypointReachedDistance = 0.1f;
+    public DetectionZone biteDetectionZone;
+    public List<Transform> waypoints;
+
+    Animator animator;
+    Rigidbody2D rb;
+    Damageable damageable;
+
+    Transform nextWaypoint;
+    int waypointNum = 0;
+
+    public bool _hasTarget = false;
+    
+
+    public bool HasTarget
+    {
+        get
+        {
+            return _hasTarget;
+        }
+        private set
+        {
+            _hasTarget = value;
+            animator.SetBool(AnimationStrings.hasTarget, value);
+        }
+
+    }
+
+    public bool CanMove 
+    {
+        get
+        {
+            return animator.GetBool(AnimationStrings.canMove); 
+        }
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        damageable = GetComponent<Damageable>();
+    }
+
+    private void Start()
+    {
+        nextWaypoint = waypoints[waypointNum];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HasTarget = biteDetectionZone.detectedColliders.Count > 0;
+    }
+    
+    private void FixedUpdate()
+    {
+        if (damageable.IsAlive)
+        {
+            if (CanMove)
+            {
+                Flight();
+            }
+            else
+            {
+                rb.velocity = Vector3.zero;
+            }
+        }
+    }
+
+    private void Flight()
+    {
+        Vector2 directionToWaypoint = (nextWaypoint.position - transform.position).normalized;
+
+        float distance = Vector2.Distance(directionToWaypoint, transform.position);
+
+        rb.velocity = directionToWaypoint * flightSpeed;
+
+        if (distance <= waypointReachedDistance)
+        {
+            waypointNum++;
+
+            if (waypointNum >= waypoints.Count)
+            {
+                waypointNum = 0;
+            }
+
+            nextWaypoint = waypoints[waypointNum];
+        } 
+    }
+}
