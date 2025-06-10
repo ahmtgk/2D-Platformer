0ea95319b17f3287cc498bd4b0fca48ab8a8fb78
ahmtgk <gk.ahmet6@gmail.com>
2025-06-10 20:01:42 +0300

    add the audio

--- Assets/Scripts/StateMachine/PlayOneShotBehaviour.cs
+++ Assets/Scripts/StateMachine/PlayOneShotBehaviour.cs
@@ -4,33 +4,47 @@ using UnityEngine;
 
 public class PlayOneShotBehaviour : StateMachineBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    public AudioClip soundToPlay;
+    public float volume = 1f;
+    public bool playOnEnter = true, playOnExit = false, playAfterDelay = false;
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    public float playDelay = 0.25f;
+    private float timeSinceEntered = 0;
+    private bool hasDElayedSoundPlayed = false;
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (playOnEnter)
+        {
+            AudioSource.PlayClipAtPoint(soundToPlay, animator.gameObject.transform.position, volume);
+        }
 
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
+        timeSinceEntered = 0f;
+        hasDElayedSoundPlayed = false;
+    }
 
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (playAfterDelay && !hasDElayedSoundPlayed)
+        {
+            timeSinceEntered += Time.deltaTime;
+
+            if (timeSinceEntered > playDelay)
+            {
+                AudioSource.PlayClipAtPoint(soundToPlay, animator.gameObject.transform.position, volume);
+                hasDElayedSoundPlayed = true;
+            }
+        }
+        
+    }
+
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+         if (playOnExit)
+        {
+            AudioSource.PlayClipAtPoint(soundToPlay, animator.gameObject.transform.position, volume); 
+        }
+    }
 }
